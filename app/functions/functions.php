36718be29533d57ddc2d740cb5d7ff663295a29e
vuviy <vova.banudz@gmail.com>
2026-01-07 11:23:39 +0200

    first commit

---
+++ app/functions/functions.php
@@ -0,0 +1,16 @@
+<?php
+
+function dd(mixed ...$args): void
+{
+    echo '<pre>';
+    foreach ($args as $arg) {
+        if (is_countable($arg)) {
+            print_r($arg);
+        } else {
+            var_dump($arg);
+        }
+        echo "\n";
+    }
+    echo '</pre>';
+    die();
+}
